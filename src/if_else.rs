f9b06a4da4fe65a915fcd154a09d93d430491503
Akash <73471799+AkashAkash311@users.noreply.github.com>
2024-09-28 20:37:21 +0530

    if else

---
+++ src/if_else.rs
@@ -0,0 +1,12 @@
+
+fn if_elsefn(){
+    let x: i32 = 5;
+
+    if x==5 {
+        println!("I'm x yk {}", x)
+    }
+}
+
+pub fn main_if_else(){
+    if_elsefn();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+mod funtions;
+mod if_else;
+
+
 fn main() {
     let x: i32 = 500;
     let y: i32 = 50000;
     let z = 500.003;
 
-    print!("{}", x)
+    println!("{}, {}, {}", x, y, z);
+
+    funtions::my_function();
+    if_else::main_if_else();
 }
